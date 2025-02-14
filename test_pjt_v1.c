1b977ecf0ece81fe8ee8eeec7b934dd3b745222e
Vishnuprasad C <94431536+Vishnuprasadc369@users.noreply.github.com>
2025-02-14 16:36:08 +0530

    Update test_pjt_v1.c

--- test_pjt_v1.c
+++ test_pjt_v1.c
@@ -1,137 +1,112 @@
 /*
- * File:   test_project_eeprom_v2.c
+ * File:   test_pjt_v2.c
  * Author: Hp
  *
- * Created on 16 December, 2024, 10:29 AM
- * added eeprom to the pjt.
+ * Created on 14 February, 2025, 2:21 PM
  */
 
-
-#define F_CPU 1000000UL//unsigned long frequency range
+#define F_CPU 8000000UL//unsigned long frequency range
 #include <avr/io.h>//input output 
 #include <util/delay.h> // delay func
 #include <stdio.h>
 #include <avr/eeprom.h>
-
 // Macros for controlling RS and Enable pins
 #define RS 0 // RS connected to B0
 #define EN 1 // Enable connected to B1
 
-void cmd(char a)
-{
-    // Send higher nibble of the command
-    PORTB = (PORTB & 0x0F) | (a & 0xF0);
-    PORTB &= ~(1 << RS); // RS = 0 (command mode)
-    PORTB |= (1 << EN);  // Enable = 1
-    _delay_ms(5);
-    PORTB &= ~(1 << EN); // Enable = 0
+// Initialize USART with LINBTR-based baud rate calculation
+void USART_Init() {
+    // Read LINBTR register and apply masking
+    DDRD = DDRD | 0x08;
+    //LINCR=0X80;
+    //while((LINCR&(0X80)));
+    LINDLR=0x00; 
+    LINBRRL = 25 ;        // Set low byte of UBRR 25 for 8mhz 51 for 16mhz
+    // Enable LIN/USART in UART mode
+    LINCR = ((1 << LENA) | (1 << LCMD0) | (1 << LCMD2));
 
-    // Send lower nibble of the command
-    PORTB = (PORTB & 0x0F) | (a << 4);
-    PORTB &= ~(1 << RS); // RS = 0 (command mode)
-    PORTB |= (1 << EN);  // Enable = 1
-    _delay_ms(5);
-    PORTB &= ~(1 << EN); // Enable = 0
 }
 
-void data(char b)
-{
-    // Send higher nibble of the data
-    PORTB = (PORTB & 0x0F) | (b & 0xF0);
-    PORTB |= (1 << RS);  // RS = 1 (data mode)
-    PORTB |= (1 << EN);  // Enable = 1
-    _delay_ms(5);
-    PORTB &= ~(1 << EN); // Enable = 0
-
-    // Send lower nibble of the data
-    PORTB = (PORTB & 0x0F) | (b << 4);
-    PORTB |= (1 << RS);  // RS = 1 (data mode)
-    PORTB |= (1 << EN);  // Enable = 1
-    _delay_ms(5);
-    PORTB &= ~(1 << EN); // Enable = 0
+// Transmit a single character using LIN/USART
+void USART_Transmit(char data) {
+    LINDAT = data;                              // Load data into LIN data register
+    while (!(LINSIR & (1 << LTXOK)));           // Wait for transmission to complete
+    LINSIR |= (1 << LTXOK);                     // Clear the transmit complete flag
 }
-
-void display(const char *p)
-{
-
-    while ((*p != 0)) {
-        data(*p);
-        p++;
-    
+void display(const char *ptr){
+    while(*ptr){
+    USART_Transmit(*ptr);
+    ptr++;
     }
+            
 }
 
-
- 
 uint32_t map_adc_to_freq(uint16_t adc_value,uint16_t max,uint16_t min) {
-
     return 50000 - ((uint32_t)(adc_value - min) * (50000 - 20000) / (max - min));
-
 }
 
 
-
-void set_pwm_freq(uint32_t freq) {
-    uint16_t prescaler_bits = 0;
+void set_pwm_freq_phase(uint32_t freq) {
     uint16_t prescaler = 1;
+    uint16_t prescaler_bits = 0;
 
-//    // Select appropriate prescaler
-//    if (freq >= 20000) {  // Prescaler 1
-//        prescaler = 1;
-//        prescaler_bits = (1 << CS10);
-//    } else if (freq >= 3125) {  // Prescaler 8
-//        prescaler = 8;
-//        prescaler_bits = (1 << CS11);
-//    } else if (freq >= 390) {  // Prescaler 64
-//        prescaler = 64;
-//        prescaler_bits = (1 << CS11) | (1 << CS10);
-//    } else if (freq >= 98) {  // Prescaler 256
-//        prescaler = 256;
-//        prescaler_bits = (1 << CS12);
-//    } else {  // Prescaler 1024
-//        prescaler = 1024;
-//        prescaler_bits = (1 << CS12) | (1 << CS10);
-//    }
+    // Select appropriate prescaler
+    if (freq >= 3906) { // Prescaler = 1
+        prescaler = 1;
+        prescaler_bits = (1 << CS10);
+    } else if (freq >= 488) { // Prescaler = 8
+        prescaler = 8;
+        prescaler_bits = (1 << CS11);
+    } else if (freq >= 61) { // Prescaler = 64
+        prescaler = 64;
+        prescaler_bits = (1 << CS11) | (1 << CS10);
+    } else if (freq >= 15) { // Prescaler = 256
+        prescaler = 256;
+        prescaler_bits = (1 << CS12);
+    } else { // Prescaler = 1024
+        prescaler = 1024;
+        prescaler_bits = (1 << CS12) | (1 << CS10);
+    }
 
     // Calculate ICR1
-    uint16_t icr = (uint16_t)((F_CPU / (freq * prescaler)) - 1);
+    uint16_t icr = (uint16_t)((F_CPU) / (2 * prescaler * freq));
 
-    // Configure Timer1 for Fast PWM mode
-    TCCR1A = (1 << WGM11)|(1 << COM1B1)|(1 << COM1B0);                // Fast PWM mode, non-inverted
-    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
-    ICR1 = icr;                           // Set TOP value for desired frequency
-    OCR1B =(uint16_t) icr *.5;                      // 50% duty cycle
+    // Configure Timer1 for Phase-Correct PWM
+    TCCR1A = (1 << WGM11) | (1 << COM1B1);       // Phase-correct PWM, clear OC1B on compare match
+    TCCR1B = (1 << WGM13) | prescaler_bits;      // Phase-correct PWM, selected prescaler
+    ICR1 = icr;                                  // Set TOP value for desired frequency
+    OCR1B = icr / 2;                             // 50% duty cycle
 }
- 
 int main(void) {
     uint16_t  eeprom_address_MIN = 1;  
     uint16_t  eeprom_address_MAX = 4;
-    uint16_t a;
+    uint16_t  eeprom_address_threshold = 8;
+    uint16_t a=0;
+    uint16_t b=0;
     uint16_t max_val = 31; // by default.
     uint16_t min_val = 3; // by default.
+    uint16_t threshold = 920; // by default.
+    
     min_val = eeprom_read_word(&eeprom_address_MIN);
     max_val = eeprom_read_word(&eeprom_address_MAX);
+    threshold = eeprom_read_word(&eeprom_address_threshold);
     
     uint8_t max_flag=0;
     uint8_t min_flag=0;
-    
+    uint32_t sum = 0;
     uint32_t freq;
     char buffer[150];
-    
-
+     // Configure Timer1 for Fast PWM mode
+    TCCR1A = (1 << WGM11)|(1 << COM1B1)|(1 << COM1B0);        // Prescaler 1        // Fast PWM mode, non-inverted
+    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
+   
     DDRD=0x00;
-    DDRB = 0xFF; // Set PORTB as output
+    
     DDRE |= (1 << PE1);// set pwm pin timer 0
     DDRC|=(1<<PC1); // set pwm pin timer 1
-    
-
-    cmd(0x02); // Initialize in 4-bit mode
-    cmd(0x28); // 4-bit mode, 2-line display
-    cmd(0x01); // Clear display
-    cmd(0x06); // Increment cursor
-    cmd(0x0E); // Display ON, Cursor ON
-
-    
+    DDRC&= ~(1 << PC3); 
+  
+    USART_Init();
 
     while(1){
         
@@ -142,39 +117,41 @@ int main(void) {
 //    ADCSRB=0x20;
 //        
     // ADC Initialization with external opam
-    ADMUX = 0xC2;  
+    ADMUX = 0x42;  
     ADCSRA = 0x87; // Enable ADC and set prescaler to 128 
-    ADCSRB=0x20;
-    
-        ADCSRA |= (1 << ADSC); 
-        while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
-
+    ADCSRB=0x00;
+    sum=0;
+    for(int i=1;i<100;i++){
+        ADCSRA |= (1 << ADSC); //01
+      //  while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
         while((ADCSRA&0x40));
         a=ADC;
-       
-      
-           
+        sum+=a;  
+    }
+        a = sum / 100; // Compute average
+//        
+//        
+//        
+//             ADCSRA |= (1 << ADSC); //01
+//      //  while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
+//        while((ADCSRA&0x40));
+//        a=ADC;
+        
+        
                if(((PIND&(0x80))==0)) {
-                   
-                   
-                    ADCSRA=0XC0;
-                    while((ADCSRA&0x40)==0);
-                    a=ADCL;
-                    a=a+(ADCH<<8);
-                    cmd(0x01);
-                    sprintf(buffer, "-Ve min=%d", a);
+
+                    
+                    sprintf(buffer, "-Ve min=%d   \r\n", a);
                     display(buffer);
-                 
                     eeprom_update_word(&eeprom_address_MIN,a);
                     min_val = eeprom_read_word(&eeprom_address_MIN);
-                    
                    
                }// -ve
                
                if((PIND&(0x40))==0) 
                {    
-                    cmd(0x01);
-                    sprintf(buffer, "+Ve max=%d", a );
+                    
+                    sprintf(buffer, "+Ve max=%d   \r\n", a );
                     display(buffer);
                     eeprom_update_word(&eeprom_address_MAX,a);
                     max_val = eeprom_read_word(&eeprom_address_MAX);
@@ -186,25 +163,57 @@ int main(void) {
 
         // Set frequency
         
-       set_pwm_freq(freq);
+       set_pwm_freq_phase(freq);
+       
+       
+ ///////////////////ad10 reading for bjt
+       
+       
+    ADMUX = 0xCA;  
+    ADCSRA = 0x87; // Enable ADC and set prescaler to 128 
+    ADCSRB=0x20;
+
+        ADCSRA |= (1 << ADSC); //01
+      //  while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
+       sum=0;
+    for(int i=1;i<100;i++){
+        ADCSRA |= (1 << ADSC); //01
+      //  while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
+        while((ADCSRA&0x40));
+        b=ADC;
+        sum+=b;  
+    }
+        b = sum / 100;
         
+         if((PINC&(1<<PC3))==0x08)
+               {    
+                
+                    sprintf(buffer, "threshold max=%d   \r\n", b);
+                    display(buffer);
+                    eeprom_update_word(&eeprom_address_threshold,b);
+                    threshold = eeprom_read_word(&eeprom_address_threshold);
+                   
+               }// +ve
+        
+        if(b>=threshold){
+            PORTB=(1<<PB2);
+            
+        }else{
+            PORTB &= ~(1 << PB2); // Clear PB2
+        }
+   
         
         // Send ADC and frequency via UART
-//       
-//        cmd(0x80);
-//        sprintf(buffer, "ADC=% 4u F% 5lu Hz", a, freq);
-//        display(buffer);
-//        cmd(0xC0);
-//        sprintf(buffer, "max% 4u min% 4u",max_val,min_val);
+       
+        
+        sprintf(buffer, "ADC10=% 4u ADC2=% 4u \r\n", b,a);
+        display(buffer);
+
+//        sprintf(buffer, "max% 4u min% 4u \r\n",max_val,min_val);
 //        display(buffer);  
-////      
-       _delay_ms(10);
-       
-  
-   
-       
-       
-       
+//      
+   //    _delay_ms(10);
+         
        
     }
     
